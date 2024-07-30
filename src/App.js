3cfe599251415b42a1c17bcb2701204c337323e7
Jack Anderson <whotfisjax@gmail.com>
2024-07-30 00:13:18 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 import PfpMaker from './PfpMaker';
 
+const CopyIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
+  </svg>
+);
+
 function App() {
+  const [copied, setCopied] = useState(false);
   const [currentImage, setCurrentImage] = useState(1);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('oming soon...');
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,13 +46,13 @@ function App() {
     <div className=''>
       <div className="h-screen w-screen flex flex-col justify-between bg-[#0a192f] overflow-clip text-white">
           <div className='absolute top-5 left-5 flex justify-center items-center z-10'>
-            <a href="https://x.com/" className=''>
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-10 md:size-12 md:hover:scale-105 transition ease-in-out duration-150' fill="#FFFFFF" viewBox="0 0 50 50">
+            <a href="https://x.com/pfpdog" className=''>
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-10 md:size-12 md:hover:scale-105 transition ease-in-out duration-150' fill="#facc15" viewBox="0 0 50 50">
                 <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
               </svg>
             </a>
             <a href="https://t.me/" className=''>
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-10 md:size-12 md:hover:scale-105 transition ease-in-out duration-150' fill="#FFFFFF" viewBox="0 0 50 50">
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-10 md:size-12 md:hover:scale-105 transition ease-in-out duration-150' fill="#facc15" viewBox="0 0 50 50">
                 <path d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path>
               </svg>
             </a>
@@ -51,7 +64,7 @@ function App() {
           </Marquee>
         </div>
 
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-center">
           <div className="w-[45%] md:w-[25%] aspect-square relative">
             {[...Array(16)].map((_, index) => (
               <img
@@ -64,7 +77,7 @@ function App() {
               />
             ))}
           </div>
-          <span className="mt-4 text-4xl md:text-6xl font-custom">pfp</span>
+          <span className="mt-4 text-4xl md:text-6xl font-custom text-center">pfp</span>
         </div>
 
         <div className='w-[120%] rotate-[30deg] -translate-x-[15%] -translate-y-[10%]'>
@@ -72,6 +85,21 @@ function App() {
             {bottomMarqueeImages}
             {bottomMarqueeImages} {/* Duplicate for seamless loop */}
           </Marquee>
+        </div>
+        <div 
+          className='absolute bottom-5 right-5 flex justify-center'
+        >
+          <div className='flex flex-col sm:flex-row justify-center bg-slate-100 rounded-xl md:rounded-full z-10 items-center gap-1 md:gap-3 px-5 py-3 max-w-full border-2 border-yellow-300'>
+            <button
+              onClick={handleCopy}
+              className="text-sm bg-yellow-400 md:hover:bg-yellow-500 transition duration-150 ease-in-out text-black py-2 px-4 rounded-full border-2 border-yellow-300 z-10 whitespace-nowrap"
+            >
+              {copied ? 'Copied!' : <CopyIcon />}
+            </button>
+            <div className='text-xs md:text-xl overflow-x-auto whitespace-nowrap font-custom text-black'>
+            coming soon...
+            </div>
+          </div>
         </div>
       </div>
       <div className='h-screen w-screen flex-col justify-center items-center gap-12 hidden md:flex border-t-2 bg-[#0a192f] text-white'>
